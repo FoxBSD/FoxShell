f94093434e77d749c9ef0b555f4f6c2df0c925b2
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-12-14 20:33:58 -0300

    refator:(main.go)

--- main.go
+++ main.go
@@ -9,14 +9,11 @@ import(
 )
 
 func main(){
-    reader := bufio.NewReader(os.Stdin) // Começamos chamando um leitor para ler os.Stdin
-    
     for { // Em um loop infinito
-
+        reader := bufio.NewReader(os.Stdin) // Começamos chamando um leitor para ler os.Stdin
+    
         fmt.Print("$ ") // Mostre o prompt atual
         input, _ := reader.ReadString('\n') // Leia o input do usuário, até ler uma quebra delinha
-
-        runInBackground := false // O estado inicial de runInBackground é falsa
 
         args := strings.Fields(strings.TrimSpace(input)) // Divida o input do usuário em chamadas e argumentos numa divisão por espaços
 
@@ -24,14 +21,6 @@ func main(){
             os.Exit(0)
         }
         
-        // Caso o ultimo do valor dos argumentos seja &, runInBackground passa a ser verdadeiro
-
-        if len(args) > 1 && args[len(args)-1] == "&"{
-            runInBackground = true
-
-            args = args[:(len(args) - 1)]
-        }
-
         switch(args[0]){ // Chamamos um switch para o primeiro valor de args
         case "cd": // Caso seja cd mudamos de diretorio
             if len(args) == 2{
@@ -40,6 +29,16 @@ func main(){
                 fmt.Println("Use cd [diretorio]")
             }
         default: // Em default executamos um comando
+
+            runInBackground := false // O estado inicial de runInBackground é falsa
+            
+            // Caso o ultimo do valor dos argumentos seja &, runInBackground passa a ser verdadeiro
+            if len(args) > 1 && args[len(args)-1] == "&"{
+                runInBackground = true
+    
+                args = args[:(len(args) - 1)]
+            }
+
             cmd := exec.Command(args[0], args[1:]...) 
             cmd.Stdin = os.Stdin
             cmd.Stdout = os.Stdout
