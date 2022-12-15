40804d1cc718f5f52d3405c41af7e42670efa2eb
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2022-12-15 11:09:20 -0300

    refactor(main.go) using strings.HasSuffix unstead strings.Contains to capture & signal to execute in background

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import(
     "bufio"
     "fmt"
+    "log"
     "os"
     "os/exec"
     "strings"
@@ -15,22 +16,28 @@ func main(){
         fmt.Print("$ ") // Mostre o prompt atual
         input, _ := reader.ReadString('\n') // Leia o input do usuário, até ler uma quebra delinha
         
-        if strings.Contains(input, "&"){ // Verifique se a string contém algum & antes do final
+        if strings.HasSuffix(input, "&"){ // Verifique se a string contém algum & antes do final
             input = strings.TrimSuffix(input, "&") // Se houver remova-o
         }
 
         args := strings.Fields(strings.TrimSpace(input)) // Divida o input do usuário em chamadas e argumentos numa divisão por espaços
 
-        if len(args) == 1 && (args[0] == "exit" || args[0] == "quit"){ // Caso o Input tenha 1 valor e seja exit ou quit, saia do shell
-            os.Exit(0)
+        if len(args) == 0 { // Se nada for digitado continue
+            continue
         }
         
         switch(args[0]){ // Chamamos um switch para o primeiro valor de args
+        case "exit","quit": // Caso o Input tenha 1 valor e seja exit ou quit, saia do shell
+            return
+
         case "cd": // Caso seja cd mudamos de diretorio
             if len(args) == 2{
-                os.Chdir(args[1])
+                err := os.Chdir(args[1])
+                if err != nil{
+                    log.Printf("Error changing directory: %v\n", err)
+                }
             }else{
-                fmt.Println("Use cd [diretorio]")
+                log.Println("Use cd [diretorio]")
             }
         default: // Em default executamos um comando
 
@@ -52,14 +59,14 @@ func main(){
             if runInBackground{
                 err := cmd.Start()
                 if err != nil{
-                    fmt.Println(err)
+                    log.Printf("Error running command in background: %v\n", err)
                 }else{
-                    fmt.Println("Running command in the background: ", strings.Join(args, " "))
+                    log.Printf("Running command in the background: %s\n", strings.Join(args, " "))
                 }
             }else{ // Caso contrário rodar em foreground
                 err := cmd.Run()
                 if err != nil{
-                    fmt.Println(err)
+                    log.Printf("Error running command: %v\n", err)
                 }
             }
         }
